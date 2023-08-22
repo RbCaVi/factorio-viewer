93483cad598b1ad0217fc4bc1a79f6d24f31dcbc
RbCaViTerminal <robert@robertvail.info>
2023-08-22 14:45:38 -0400

    move some promise functions to util

--- image.js
+++ image.js
@@ -39,30 +39,6 @@ function loadImage(src){
     })
 }
 
-function promiseChain(ps,f){
-    // ps is an array of promises
-    // f is a one argument function
-    // it calls f on the data from each promise in order and returns the resulting promise
-    return ps.reduce((p1,p2)=>{
-        return p1.then(data1=>{
-            f(data1);
-            return p2;
-        })
-    }).then(data=>{
-        f(data);
-    });
-}
-
-function packPromise(p,data){
-    // puts a data value into a promise
-    return p.then(pdata=>[pdata,data]);
-}
-
-function makePromise(data){
-    // returns a promise that returns this data
-    return new Promise(resolve=>resolve(data));
-}
-
 function colorToString(color){
     return color.reduce((s, channel) => {
         var c = Math.floor(channel).toString(16);
--- util.js
+++ util.js
@@ -148,8 +148,32 @@ util.entitytypes=[
   "speech-bubble",
   "sticker",
   "tile-ghost"
-]
+];
 
-util.difficulty=['normal','expensive']
+util.difficulty=['normal','expensive'];
 
-export {clone,util,craftertypes};
+function promiseChain(ps,f){
+    // ps is an array of promises
+    // f is a one argument function
+    // it calls f on the data from each promise in order and returns the resulting promise
+    return ps.reduce((p1,p2)=>{
+        return p1.then(data1=>{
+            f(data1);
+            return p2;
+        })
+    }).then(data=>{
+        f(data);
+    });
+}
+
+function packPromise(p,data){
+    // puts a data value into a promise
+    return p.then(pdata=>[pdata,data]);
+}
+
+function makePromise(data){
+    // returns a promise that returns this data
+    return new Promise(resolve=>resolve(data));
+}
+
+export {clone,util,craftertypes,promiseChain,packPromise,makePromise};
