80d2ba4c90f8ce03aa260af4059e698a7f718991
RbCaViTerminal <robert@robertvail.info>
2023-08-25 18:44:05 -0400

    simplexsolver class + recipes ranked by tech tree position + autogen mining 'recipes'

--- simplex.js
+++ simplex.js
@@ -1,7 +1,5 @@
 import {clone} from './util.js';
 
-let rtable={};
-
 class Rational{
 	constructor(num,denom=1){
 		var n1,d1,n2,d2;
@@ -158,199 +156,228 @@ function mult(r1,r2){
 	return r;
 }
 
-function unallowed(recipe,data) {
-	if(recipe.name=='empty-barrel'){
-		return false;
-	}
-	if(recipe.name=='se-matter-fusion-dirty'){
-		return false;
-	}
-	if(recipe.name=='se-big-turbine-internal'){
-		console.log('rejected',recipe,'by turbine-internal');
-		return true;
-	}
-	if(recipe.name=='se-bio-methane-to-crude-oil'){
-		console.log('rejected',recipe,'by bio-crude');
-		return true;
-	}
-	if(recipe.name=='se-bio-sludge-crude-oil'){
-		console.log('rejected',recipe,'by bio-crude');
-		return true;
-	}
-	if(recipe.name=='coal-liquefaction'){
-		console.log('rejected',recipe,'by coal');
-		return true;
-	}
-	if(recipe.name.startsWith('se-core-fragment-')){
-		console.log('rejected',recipe,'by core');
-		return true;
-	}
-	if(recipe.name.includes('naq')){
-		console.log('rejected',recipe,'by naquium');
-		return true;
-	}
-	if(recipe.name.includes('cry')){
-		console.log('rejected',recipe,'by cryonite');
-		return true;
-	}
-	if(recipe.name.includes('vul')){
-		console.log('rejected',recipe,'by vulcanite');
-		return true;
-	}
-	if(recipe.name.includes('beryl')){
-		console.log('rejected',recipe,'by beryllium');
-		return true;
-	}
-	if(recipe.name.includes('vita')){
-		console.log('rejected',recipe,'by vitamelange');
-		return true;
-	}
-	if(recipe.name.includes('holm')){
-		console.log('rejected',recipe,'by holmium');
-		return true;
-	}
-	if(recipe.name.includes('iri')){
-		console.log('rejected',recipe,'by iridium');
-		return true;
-	}
-	if(recipe.name.includes('scrap')){
-		console.log('rejected',recipe,'by scrap');
-		return true;
-	}
-	if(recipe.name.includes('wood')||recipe.name.includes('bio')||recipe.name.includes('spec')){
-		console.log('rejected',recipe,'by biology');
-		return true;
-	}
-	if(recipe.name.startsWith('se-condenser-turbine-reclaim-water-')){
-		console.log('rejected',recipe,'by turbine-reclaim');
-		return true;
-	}
-	if(recipe.name.startsWith('se-melting-')){
-		console.log('rejected',recipe,'by ice');
-		return true;
-	}
-	if(recipe.name.startsWith('se-matter-fusion-')){
-		console.log('rejected',recipe,'by fusion');
-		return true;
-	}
-	if(recipe.name.startsWith('empty-')&&recipe.name.endsWith('-barrel')){
-		console.log('rejected',recipe,'by barrel');
-		return true;
-	}
-	if(recipe.category!='hard-recycling'){
-		//return false;
-	}
-	if(recipe.normal.ingredients.length==1){
-		for(var precipe of data.produces.normal[recipe.normal.ingredients[0][0]]??[]){
-			if(data.pdata.recipe[precipe].normal.results.length>1){
+class SimplexSolver{
+	constructor(data){
+		this.data=data;
+		this.reciperanks=rankrecipes(this.data);
+		this.rtable={}
+		for(var recipename in this.data.pdata.recipe){
+			var recipe=this.data.pdata.recipe[recipename];
+			if(this.#unallowed(recipe)){
 				continue;
 			}
-			var icounts={};
-			for(var [ing,count] of data.pdata.recipe[precipe].normal.ingredients){
-				icounts[ing]=(icounts[ing]??0)+count;
-			}
-			var rcounts={};
-			for(var [res,count] of data.pdata.recipe[precipe].normal.results){
-				rcounts[res]=(icounts[res]??0)+count;
-			}
-			var pass=true;
-			for(var [res,count] of recipe.normal.results){
-				if((count/recipe.normal.ingredients[0][1])>=((icounts[res]??0)/rcounts[recipe.normal.ingredients[0][0]])){
-					pass=false;
-					break;
+			var entry={};
+			for(var ing of recipe.normal.ingredients){
+				if(!(ing[0] in entry)){
+					entry[ing[0]]=new Rational(0);
 				}
+				entry[ing[0]].sub(div(ing[1],recipe.normal.time));
 			}
-			if(pass){
-				console.log('rejected',recipe,'by',data.pdata.recipe[precipe]);
+			for(var res of recipe.normal.results){
+				if(!(res[0] in entry)){
+					entry[res[0]]=new Rational(0);
+				}
+				entry[res[0]].add(div(res[1],recipe.normal.time));
+			}
+			this.rtable[recipename]=entry;
+		}
+
+		for(var resource of Object.values(this.data.data.resource)){
+			var entry={};
+			if((resource.fluid_amount??0)>0){
+				entry[resource.required_fluid]=div(-resource.fluid_amount,resource.mining_time);
+			}
+			if(resource.results){
+				for(var res of resource.results){
+					if(!(res[0] in entry)){
+						entry[res[0]]=new Rational(0);
+					}
+					entry[res[0]].add(div(res[1],resource.mining_time));
+				}
+			}else if(resource.result){
+				if(!(resource.result in entry)){
+					entry[resource.result]=new Rational(0);
+				}
+				entry[resource.result].add(div(resource.count,resource.mining_time));
+			}
+			this.rtable['mine.'+item]=entry;
+		}
+	}
+
+	#unallowed(recipe,data) {
+		/*
+			if(recipe.name=='empty-barrel'){
+				return false;
+			}
+			if(recipe.name=='se-matter-fusion-dirty'){
+				return false;
+			}
+			if(recipe.name=='se-bio-methane-to-crude-oil'){
+				console.log('rejected',recipe,'by bio-crude');
 				return true;
 			}
-		}
-	}
-	return false;
-}
-
-function setup(data){
-	rtable={}
-	for(var recipename in data.pdata.recipe){
-		var recipe=data.pdata.recipe[recipename];
-		if(unallowed(recipe,data)){
-			continue;
-		}
-		var entry={};
-		for(var ing of recipe.normal.ingredients){
-			if(!(ing[0] in entry)){
-				entry[ing[0]]=new Rational(0);
+			if(recipe.name=='se-bio-sludge-crude-oil'){
+				console.log('rejected',recipe,'by bio-crude');
+				return true;
 			}
-			entry[ing[0]].sub(div(ing[1],recipe.normal.time));
-		}
-		for(var res of recipe.normal.results){
-			if(!(res[0] in entry)){
-				entry[res[0]]=new Rational(0);
+			if(recipe.name=='coal-liquefaction'){
+				console.log('rejected',recipe,'by coal');
+				return true;
 			}
-			entry[res[0]].add(div(res[1],recipe.normal.time));
-		}
-		rtable[recipename]=entry;
-	}
-
-	for(var item of ['iron-ore','copper-ore','crude-oil']){
-		var entry={};
-		entry[item]=new Rational(1);
-		rtable['mine.'+item]=entry;
-	}
-}
-
-function clonertable(){
-	var newtable={};
-	for(var [key,recipe] of Object.entries(rtable)){
-		var newrecipe={};
-		for(var [item,amount] of Object.entries(recipe)){
-			newrecipe[item]=new Rational(amount.num,amount.denom);
-		}
-		newtable[key]=newrecipe;
-	}
-	return newtable;
-}
-
-function f(outs){
-	var rtable2=clonertable();
-	addcosts(rtable2);
-	addslacks(rtable2);
-	outs=clone(outs);
-	mapKeys(outs,x=>new Rational(x));
-	rtable2['.out']=outs;
-	while(true){
-		var col=minIndex(outs);
-		if(!(outs[col].negative())){
-			break;
-		}
-		var [minrow,minr]=Object.entries(rtable2)[0];
-		for(var [row,recipe] of Object.entries(rtable2)){
-			if((!(col in recipe))||(!recipe[col].positive())||row=='.out'){
-				continue;
+			if(recipe.name.includes('naq')){
+				console.log('rejected',recipe,'by naquium');
+				return true;
 			}
-			if((!(col in minr))||(!minr[col].positive())||minrow=='.out'||(div(minr['.cost'],minr[col]).greaterthan(div(recipe['.cost'],recipe[col])))){
-				minr=recipe;
-				minrow=row;
+			if(recipe.name.includes('cry')){
+				console.log('rejected',recipe,'by cryonite');
+				return true;
 			}
+			if(recipe.name.includes('vul')){
+				console.log('rejected',recipe,'by vulcanite');
+				return true;
+			}
+			if(recipe.name.includes('beryl')){
+				console.log('rejected',recipe,'by beryllium');
+				return true;
+			}
+			if(recipe.name.includes('vita')){
+				console.log('rejected',recipe,'by vitamelange');
+				return true;
+			}
+			if(recipe.name.includes('holm')){
+				console.log('rejected',recipe,'by holmium');
+				return true;
+			}
+			if(recipe.name.includes('iri')){
+				console.log('rejected',recipe,'by iridium');
+				return true;
+			}
+			if(recipe.name.includes('scrap')){
+				console.log('rejected',recipe,'by scrap');
+				return true;
+			}
+			if(recipe.name.includes('wood')||recipe.name.includes('bio')||recipe.name.includes('spec')){
+				console.log('rejected',recipe,'by biology');
+				return true;
+			}
+			if(recipe.name.startsWith('se-melting-')){
+				console.log('rejected',recipe,'by ice');
+				return true;
+			}
+			if(recipe.name.startsWith('se-matter-fusion-')){
+				console.log('rejected',recipe,'by fusion');
+				return true;
+			}
+		*/
+		if(recipe.name=='se-big-turbine-internal'){
+			console.log('rejected',recipe,'by turbine-internal');
+			return true;
 		}
-		var v=minr[col];
-		mapKeys(minr,x=>div(x,v));
-		console.log('pivot',minrow,'by',col);
-		for(var recipe of Object.values(rtable2)){
-			if(recipe===minr){
-				continue;
-			}
-			if(col in recipe){
-				if(recipe===outs){
-					console.log('subtracting',minr,'*',recipe[col],'from .out')
+		if(recipe.name.startsWith('se-condenser-turbine-reclaim-water-')){
+			console.log('rejected',recipe,'by turbine-reclaim');
+			return true;
+		}
+		if(recipe.name.startsWith('se-core-fragment-')){
+			console.log('rejected',recipe,'by core');
+			return true;
+		}
+		if(recipe.name.startsWith('empty-')&&recipe.name.endsWith('-barrel')){
+			console.log('rejected',recipe,'by barrel');
+			return true;
+		}
+		if(recipe.category!='hard-recycling'){
+			return false;
+		}
+		if(recipe.normal.ingredients.length==1){
+			for(var precipe of this.data.produces.normal[recipe.normal.ingredients[0][0]]??[]){
+				if(this.data.pdata.recipe[precipe].normal.results.length>1){
+					continue;
 				}
-				subtractObject(recipe,minr,recipe[col]);
+				var icounts={};
+				for(var [ing,count] of this.data.pdata.recipe[precipe].normal.ingredients){
+					icounts[ing]=(icounts[ing]??0)+count;
+				}
+				var rcounts={};
+				for(var [res,count] of this.data.pdata.recipe[precipe].normal.results){
+					rcounts[res]=(icounts[res]??0)+count;
+				}
+				var pass=true;
+				for(var [res,count] of recipe.normal.results){
+					if((count/recipe.normal.ingredients[0][1])>=((icounts[res]??0)/rcounts[recipe.normal.ingredients[0][0]])){
+						pass=false;
+						break;
+					}
+				}
+				if(pass){
+					console.log('rejected',recipe,'by',this.data.pdata.recipe[precipe]);
+					return true;
+				}
 			}
 		}
-		console.log(s(outs));
+		return false;
 	}
-	mapKeys(outs,f=>f.reduce());
-	return outs;
+
+	#clonertable(rank){
+		var newtable={};
+		for(var [key,recipe] of Object.entries(this.rtable)){
+			if(this.reciperanks[key]>rank){
+				continue;
+			}
+			var newrecipe={};
+			for(var [item,amount] of Object.entries(recipe)){
+				newrecipe[item]=new Rational(amount.num,amount.denom);
+			}
+			newtable[key]=newrecipe;
+		}
+		return newtable;
+	}
+
+	#getitemrank(item){
+		return Math.min(...this.data.produces.normal[item].map(recipe=>this.reciperanks[recipe]));
+  }
+
+	solve(outs){
+		var rank=Math.max(...Object.keys(outs).map(item=>this.#getitemrank(item)??0));
+		var rtable2=this.#clonertable(rank);
+		addcosts(rtable2);
+		addslacks(rtable2);
+		outs=clone(outs);
+		mapKeys(outs,x=>new Rational(x));
+		rtable2['.out']=outs;
+		while(true){
+			var col=minIndex(outs);
+			if(!(outs[col].negative())){
+				break;
+			}
+			var [minrow,minr]=Object.entries(rtable2)[0];
+			for(var [row,recipe] of Object.entries(rtable2)){
+				if((!(col in recipe))||(!recipe[col].positive())||row=='.out'){
+					continue;
+				}
+				if((!(col in minr))||(!minr[col].positive())||minrow=='.out'||(div(minr['.cost'],minr[col]).greaterthan(div(recipe['.cost'],recipe[col])))){
+					minr=recipe;
+					minrow=row;
+				}
+			}
+			var v=minr[col];
+			mapKeys(minr,x=>div(x,v));
+			console.log('pivot',minrow,'by',col);
+			for(var recipe of Object.values(rtable2)){
+				if(recipe===minr){
+					continue;
+				}
+				if(col in recipe){
+					if(recipe===outs){
+						console.log('subtracting',minr,'*',recipe[col],'from .out')
+					}
+					subtractObject(recipe,minr,recipe[col]);
+				}
+			}
+			console.log(s(outs));
+		}
+		mapKeys(outs,f=>f.reduce());
+		return outs;
+	}
 }
 
 function minIndex(o) {
@@ -403,4 +430,51 @@ function addslacks(rtable) {
 	}
 }
 
-export {f,setup,rtable};
+function ranktech(data) {
+	var techpres=new Map((
+		function*() {
+		  for (let key in data.data.technology){
+		    yield [key, data.prereqs.normal[key]??[]];
+		  }
+		})()
+	);
+	var ranked={};
+	var allranked=new Set();
+	for(var i=0;;i++){
+		var newranks=[];
+		for(var [techname,prereqs] of techpres.entries()){
+			if(allranked.has(techname)){
+				continue;
+			}
+			if(prereqs.every(p=>allranked.has(p))){
+				newranks.push(techname);
+				ranked[techname]=i;
+			}
+		}
+		if(newranks.length==0){
+			break;
+		}
+		newranks.forEach(item=>allranked.add(item));
+	}
+	return ranked;
+}
+
+function rankrecipes(data){
+	var techrank=ranktech(data);
+	var ranks={};
+	for(var recipename in data.data.recipe){
+		var recipe=data.data.recipe[recipename];
+		if(recipe.normal.enabled??true){
+			ranks[recipename]=0;
+			continue;
+		}
+		var techs=data.unlockedby.normal[recipename];
+		if(techs==undefined){
+			continue;
+		}
+		ranks[recipename]=Math.min(...techs.map(tech=>techrank[tech]))+1;
+	}
+	return ranks;
+}
+
+export {SimplexSolver};
