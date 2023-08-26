49c72bebdebd4f7651e440e967f8b797a1b8095b
RbCaViTerminal <robert@robertvail.info>
2023-08-26 09:42:52 -0400

    split locale out of data
    the locale data and localize() are in locale.FactorioLocale
    the *locale() functions are in locale.FactorioLocalizer

--- data.js
+++ data.js
@@ -3,25 +3,12 @@ import {normalizerecipe,normalizetech} from './normalize.js';
 import {processrecipe,processtech} from './process.js';
 
 class FactorioData{
-  #localecache={
-    'recipe':{},
-    'entity':{},
-    'item':{},
-    'fluid':{},
-    'equipment':{},
-    'tech':{}
-  };
-
-  constructor(data,locale){
+  constructor(data){
     if(typeof data=='string'){
       data=JSON.parse(data);
     }
-    if(typeof locale=='string'){
-      locale=JSON.parse(locale);
-    }
     this.data=data;
     this.rawdata=clone(data);
-    this.locale=locale;
     for(key in this.data.recipe){
       this.data.recipe[key]=normalizerecipe(data.recipe[key]);
     }
@@ -109,196 +96,6 @@ class FactorioData{
     }
   }
 
-  #getnamedesc(thing,type,name){
-    var name,desc;
-    if(!name){
-      name=thing.name;
-    }
-    if('localised_description' in thing){
-      desc=this.localize(thing.localised_description);
-    }else{
-      desc=this.localize(type+'-description.'+name);
-    }
-    if('localised_name' in thing){
-      name=this.localize(thing.localised_name);
-    }else{
-      name=this.localize(type+'-name.'+name);
-    }
-    return [name,desc];
-  }
-
-  #localizeraw(s){
-    console.info('localize',s)
-    if(typeof s=='string'){
-      return s;
-    }else if(typeof s=='number'){
-      return ''+s;
-    }else if(Array.isArray(s)){
-      if(s[0]==''){
-        return s.slice(1).map(x=>this.#localizeraw(x)).join('');
-      }
-      var l=this.locale[s[0]];
-      if(l==undefined){
-        return null;
-      }
-      for(var i=1;i<s.length;i++){
-        l=l.replace('__'+i+'__',this.#localizeraw(s[i]));
-      }
-      return l;
-    }
-    return this.locale.get(s);
-  }
-
-  localize(s){
-    if(typeof s=='string'){
-      var l=this.#localizeraw([s]);
-      console.info('localized to',l);
-      return l;
-    }
-    var l=this.#localizeraw(s);
-    console.info('localized to',l);
-    return l;
-  }
-  
-  recipelocale(recipe){
-    if(recipe in this.#localecache.recipe){
-      return this.#localecache.recipe[recipe];
-    }
-    var namedesc;
-    recipe=this.data.recipe[recipe];
-    namedesc=this.#recipelocaleraw(recipe);
-    this.#localecache.recipe[recipe.name]=namedesc;
-    return namedesc;
-  }
-
-  #recipelocaleraw(recipe){
-    console.info(recipe)
-    if('normal' in recipe){
-      recipe=clone(recipe);
-      Object.assign(recipe,recipe.normal);
-      delete recipe.normal;
-    }
-    var name,desc;
-    if('localised_name' in recipe){
-      name=this.localize(recipe.localised_name);
-    }else{
-      name=this.localize('recipe-name.'+recipe.name);
-    }
-    if('localised_description' in recipe){
-      desc=this.localize(recipe.localised_description);
-    }else{
-      desc=this.localize('recipe-description.'+recipe.name);
-    }
-    if(name!=null){
-      return [name,desc];
-    }
-    if(recipe.results.length==1){
-      if('main_product' in recipe&&recipe.main_product==''){
-        return this.#getnamedesc(recipe,'recipe');
-      }
-      return this.itemlocale(recipe.results[0].name);
-    }
-    if('main_product' in recipe&&recipe.main_product!=''){
-      return this.itemlocale(recipe.main_product);
-    }
-    return this.#getnamedesc(recipe,'recipe');
-  }
-
-  entitylocale(entity){
-    if(entity in this.#localecache.entity){
-      return this.#localecache.entity[entity];
-    }
-    var namedesc;
-    for(var etype of util.entitytypes){
-      if(entity in this.data[etype]){
-        entity=this.data[etype][entity];
-        namedesc=this.#getnamedesc(entity,'entity');
-        this.#localecache.entity[entity.name]=namedesc;
-        return namedesc;
-      }
-    }
-    namedesc=[null,null];
-    this.#localecache.entity[entity.name]=namedesc;
-    return namedesc;
-  }
-
-  itemlocale(item){
-    if(item in this.#localecache.item){
-      return this.#localecache.item[item];
-    }
-    var namedesc;
-    if(item=='time'){
-      namedesc=['Time','Time'];
-      this.#localecache.item[item.name]=namedesc;
-      return namedesc;
-    }
-    item=this.getitem(item);
-    if(item.type=='fluid'){
-      namedesc=this.fluidlocale(item.name);
-      this.#localecache.item[item.name]=namedesc;
-      return namedesc;
-    }
-    namedesc=this.#getnamedesc(item,'item');
-    if('placed_as_equipment_result' in item){
-      namedesc=this.equipmentlocale(item.placed_as_equipment_result);
-    }
-    if(!namedesc[0]){
-      namedesc=this.entitylocale(item.place_result);
-    }
-    namedesc[1]=namedesc[1]??'';
-    this.#localecache.item[item.name]=namedesc;
-    return namedesc;
-  }
-
-  fluidlocale(fluid){
-    if(fluid in this.#localecache.fluid){
-      return this.#localecache.fluid[fluid];
-    }
-    var namedesc;
-    var fluid=this.data.fluid[fluid];
-    namedesc=this.#getnamedesc(fluid,'fluid');
-    this.#localecache.fluid[fluid.name]=namedesc;
-    return namedesc;
-  }
-
-  equipmentlocale(equipment){
-    if(equipment in this.#localecache.equipment){
-      return this.#localecache.equipment[equipment];
-    }
-    var namedesc;
-    for(var etype of util.equipmenttypes){
-      if(equipment in this.data[etype]){
-        equipment=this.data[etype][equipment];
-        namedesc=this.#getnamedesc(equipment,'equipment');
-        this.#localecache.equipment[equipment.name]=namedesc;
-        return namedesc;
-      }
-    }
-    namedesc=[null,null];
-    this.#localecache.equipment[equipment.name]=namedesc;
-    return namedesc;
-  }
-
-  techlocale(tech){
-    if(tech in this.#localecache.tech){
-      return this.#localecache.tech[tech];
-    }
-    var name=tech;
-    tech=this.data.technology[tech];
-    var n=+name.slice(name.lastIndexOf('-')+1);
-    var pf;
-    if(isNaN(n)||n==Infinity||n==-Infinity){
-      pf='';
-    }else{
-      pf=' '+n;
-      name=name.slice(0,name.lastIndexOf('-'));
-    }
-    var namedesc=this.#getnamedesc(tech,'technology',name);
-    namedesc[0]+=pf;
-    this.#localecache.tech[tech.name]=namedesc;
-    return namedesc;
-  }
-
   getitemtype(item){
     for(var itype of ['fluid'].concat(util.itemtypes)){
       if(item in this.data[itype]){
@@ -315,16 +112,28 @@ class FactorioData{
     }
   }
 
+  getentity(entity){
+    for(var etype of ['fluid'].concat(util.entitytypes)){
+      if(entity in this.data[etype]){
+        return this.data[etype][entity];
+      }
+    }
+  }
+
+  getequipment(equipment){
+    for(var etype of ['fluid'].concat(util.equipmenttypes)){
+      if(equipment in this.data[etype]){
+        return this.data[etype][equipment];
+      }
+    }
+  }
+
   getrawitem(item){
     for(var itype of ['fluid'].concat(util.itemtypes)){
       if(item in this.rawdata[itype]){
         return this.rawdata[itype][item];
       }
     }
-  }
-
-  getlcache(){
-    return this.#localecache;
   }
 }
 
