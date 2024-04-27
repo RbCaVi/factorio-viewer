806f001ed5e29aff862d21360f22e8b6bbd3c511
RbCaViTerminal <robert@robertvail.info>
2024-04-26 22:13:33 -0400

    rich text tags

--- renderers.js
+++ renderers.js
@@ -120,11 +120,130 @@ function splitrichtext(text) {
   return {texts,tags};
 }
 
-function richtext(self, structure, contents, options) {
-  let span=document.createElement("span");
-  const split=splitrichtext(structure.text);
-  span.textContent=JSON.stringify(split);
+// default, red, green, blue, orange, yellow, pink, purple, white, black, gray, brown, cyan, acid
+const colors={
+  red:{r:1,g:0,b:0,a:1},
+}
+
+function gettaginfo(tag){
+  tag=tag.slice(1,-1);
+  const equals=tag.indexOf('=');
+  if(equals==-1){
+    return {type:tag.trim()};
+  }
+  const type=tag.slice(0,equals).trim();
+  const value=tag.slice(equals+1).trim();
+  if(type=='color'){
+    // [color=col]...[/color]
+    // [color=r,g,b]...[/color]
+    // [color=#rrggbb]...[/color]
+    // [color=#aarrggbb]...[/color]
+    if(value in colors){
+      return {type:'color',color:colors[value]};
+    }
+    if(value.startsWith('#')){
+      // https://learnersbucket.com/examples/interview/convert-hex-color-to-rgb-in-javascript/
+      if(value.length==6){
+        const a = 1;
+        const r = parseInt(hex.slice(0, 2), 16) / 255;
+        const g = parseInt(hex.slice(2, 4), 16) / 255;
+        const b = parseInt(hex.slice(4, 6), 16) / 255;
+        return {type:'color',color:{a,r,g,b}};
+      }else{
+        const a = parseInt(hex.slice(0, 2), 16) / 255;
+        const r = parseInt(hex.slice(2, 4), 16) / 255;
+        const g = parseInt(hex.slice(4, 6), 16) / 255;
+        const b = parseInt(hex.slice(6, 8), 16) / 255;
+        return {type:'color',color:{a,r,g,b}};
+      }
+    }
+    const channels=value.split(',');
+    return {type:'color',color:{
+      a:1,
+      r:parseInt(channels[0]),
+      g:parseInt(channels[1]),
+      b:parseInt(channels[2])
+    }};
+  }
+  if(type=='font'){
+    return {type:'font',font:value};
+  }
+  return {type,value};
+}
+
+function nestrichtext(split) {
+  const {texts,tags}=split;
+  const stack=[{type:'root',contents:[]}]
+  for(let i=0;i<tags.length;i++){
+    if(texts[i].length>0){
+      stack.at(-1).contents.push(texts[i]);
+    }
+    const tag=gettaginfo(tags[i]);
+    if(tag.type=='color'){
+      stack.push({type:'color',color:tag.color,contents:[]});
+    }else if(tag.type=='font'){
+      stack.push({type:'font',font:tag.font,contents:[]});
+    }else if(tag.type=='/color'||tag.type=='/font'){
+      if(stack.at(-1).type!=tag.type.slice(1)){
+        throw 'unmatched tags';
+      }
+      const top=stack.pop();
+      stack.at(-1).contents.push(top);
+    }else{
+      stack.at(-1).contents.push(tag);
+    }
+  }
+  stack.at(-1).contents.push(texts.at(-1));
+  if(stack.length>1){
+    throw 'unmatched tags';
+  }
+  return stack[0];
+}
+
+// copied from image.js
+function colorToString(color){
+  return color.reduce((s, channel) => {
+    let c = Math.floor(channel*255).toString(16);
+    c = (c.length==1?"0":"")+c;
+    s += c;
+    return s;
+  }, "#");
+}
+
+function renderrichtext(nested) {
+  const span=document.createElement("span");
+  console.log('render',nested);
+  for(const part of nested.contents){
+    if(typeof part=='string'){
+      span.append(part);
+    }else if(part.type=='color'||part.type=='font'){
+      const span2=document.createElement("span");
+      if(part.type=='color'){
+        span2.style.color=colorToString([part.color.r,part.color.g,part.color.b,part.color.a])
+      }else{
+        span2.style.padding='15px'
+        span2.style.color='green'
+      }
+      span2.append(renderrichtext(part))
+      span.append(span2)
+    }else{
+      const span2=document.createElement("span");
+      span2.style.padding='10px'
+      span2.textContent=JSON.stringify(part);
+      span.append(span2)
+    }
+  }
   return span;
+}
+
+function richtext(self, structure, contents, options) {
+  //let span=document.createElement("span");
+  const split=splitrichtext(structure.text);
+  const nest=nestrichtext(split);
+  const out=renderrichtext(nest);
+  //span.textContent=JSON.stringify(nest);
+  //return span;
+  return out;
 }
 
 function recipetostructure(recipe, onclick) {
