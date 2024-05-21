01a792691f66fb4db3cfdd7b1dc5b334c0606c89
RbCaViTerminal <robert@robertvail.info>
2024-05-21 16:45:54 -0400

    fix some things in workers.js

--- workers.js
+++ workers.js
@@ -1,39 +1,56 @@
+let makePromise;
+
+if (Promise.withResolvers) {
+	makePromise = function makePromise() {
+		const {promise,resolve} = Promise.withResolvers();
+		return {promise,resolve};
+	}
+} else {
+	makePromise = function makePromise() {
+		let resolve;
+		const promise = new Promise(res => {
+		  resolve = res;
+		});
+		return {promise,resolve};
+	}
+}
+
 class WorkerPool {
 	constructor(workersource, workercount = 16){
-		const workers = [];
-		const queue = [];
+		this.workers = [];
+		this.queue = [];
 
 		for (let i = 0; i < workercount; i++) {
-			const worker = new Worker(workersource):
+			const worker = new Worker(workersource);
 			worker.addEventListener('message',(([data])=>{
-				const [_,_,resolve] = workers[i];
-				if (queue.length == 0) {
-					workers[i][1] = false;
+				const [,,resolve] = this.workers[i];
+				if (this.queue.length == 0) {
+					this.workers[i][1] = false;
 				} else {
-					const [data2,resolve2] = queue.pop();
-					workers[i][0].postMessage(data);
-					workers[i][2] = resolve2;
+					const [data2,resolve2] = this.queue.pop();
+					this.workers[i][0].postMessage(data);
+					this.workers[i][2] = resolve2;
 				}
 				resolve(data);
 			}));
-			workers.push([worker,false,null]);
+			this.workers.push([worker,false,null]);
 		}
 	}
 
 	run(data) { // returns a promise which fulfills when the worker sends back a message
-		for (let i = 0; i < workers.length; i++) {
-			const [worker,running,_] = workers[i];
+		for (let i = 0; i < this.workers.length; i++) {
+			const [worker,running,] = this.workers[i];
 			if (running) {
 				continue;
 			}
 			worker.postMessage(data);
-			const {promise,resolve} = Promise.withResolvers();
-			workers[i][1] = true;
-			workers[i][2] = resolve;
+			const {promise,resolve} = makePromise();
+			this.workers[i][1] = true;
+			this.workers[i][2] = resolve;
 			return promise;
 		}
-		const {promise,resolve} = Promise.withResolvers();
-		queue.push([data,resolve]);
+		const {promise,resolve} = makePromise();
+		this.queue.push([data,resolve]);
 		return promise;
 	}
 }
