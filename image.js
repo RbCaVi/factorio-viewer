5d14575707449569f1d4c0d2f4bf3eb359a121c6
RbCaViTerminal <robert@robertvail.info>
2024-05-21 17:13:07 -0400

    a thing

--- image.js
+++ image.js
@@ -4,15 +4,13 @@ let makeiconURL;
 
 if (!window.Worker) { // workers don't exist
   await import('./image-impl.js');
-  makeiconURL = window.__imagestuff__.makeiconURL;
+  makeiconURL = window.__imagestuff__.makeiconURL.bind(undefined,promiseChain,packPromise,makePromise);
 } else { // running in the window and workers exist
-  const pool = new WorkerPool('image.js'); // trickery
+  const pool = new WorkerPool('image-impl.js'); // less trickery
 
-  function _makeiconURL(data,options,size=32){
+  makeiconURL = function makeiconURL(data,options,size=32){
     return pool.run([data,options,size]);
   }
-
-  makeiconURL = _makeiconURL;
 }
 
 export {makeiconURL};
