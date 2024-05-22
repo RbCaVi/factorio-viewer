b187388339dfcdfcce0c037073c2b44122e7e8a9
RbCaViTerminal <robert@robertvail.info>
2024-05-22 15:21:10 -0400

    add another branch for worker but no offscreencanvas

--- image.js
+++ image.js
@@ -6,7 +6,7 @@ let makeiconURL;
 
 let iconcache={};
 
-if (!window.Worker || !window.OffscreenCanvas) { // workers or offscreencanvas don't exist
+if (!window.Worker) { // workers or offscreencanvas don't exist
   makeiconURL = function makeiconURL(data,options,size=32){
     let cachekey=JSON.stringify({icon:data.icon,icons:data.icons,icon_size:data.icon_size,rendered_size:size});
     if(cachekey in iconcache){
@@ -20,19 +20,49 @@ if (!window.Worker || !window.OffscreenCanvas) { // workers or offscreencanvas d
     });
   }
 } else { // workers exist
-  const pool = new WorkerPool('image-worker.js'); // no trickery
+  if (!window.OffscreenCanvas) { // no canvas, worker only loads
+    // const pool = new WorkerPool('image-worker-load.js'); // no trickery
 
-  makeiconURL = function makeiconURL(data,options,size=32){
-    let cachekey=JSON.stringify({icon:data.icon,icons:data.icons,icon_size:data.icon_size,rendered_size:size});
-    if(cachekey in iconcache){
-      console.log(cachekey);
-      return makePromise(iconcache[cachekey]);
+    // makeiconURL = function makeiconURL(data,options,size=32){
+    //   let cachekey=JSON.stringify({icon:data.icon,icons:data.icons,icon_size:data.icon_size,rendered_size:size});
+    //   if(cachekey in iconcache){
+    //     console.log(cachekey);
+    //     return makePromise(iconcache[cachekey]);
+    //   }
+    //   let canvas=window.__imagestuff__.getCanvas(size,size);
+    //   return pool.run([canvas,data,options,size],{transfer:[canvas]}).then((url)=>{
+    //     iconcache[cachekey]=url;
+    //     return url;
+    //   });
+    // }
+
+    makeiconURL = function makeiconURL(data,options,size=32){
+      let cachekey=JSON.stringify({icon:data.icon,icons:data.icons,icon_size:data.icon_size,rendered_size:size});
+      if(cachekey in iconcache){
+        console.log(cachekey);
+        return makePromise(iconcache[cachekey]);
+      }
+      let canvas=window.__imagestuff__.getCanvas(size,size);
+      return window.__imagestuff__.makeiconURL(promiseChain,packPromise,makePromise,canvas,data,options,size).then((url)=>{
+        iconcache[cachekey]=url;
+        return url;
+      });
     }
-    let canvas=window.__imagestuff__.getCanvas(size,size);
-    return pool.run([canvas,data,options,size],{transfer:[canvas]}).then((url)=>{
-      iconcache[cachekey]=url;
-      return url;
-    });
+  } else { // offscreencanvas
+    const pool = new WorkerPool('image-worker.js'); // no trickery
+
+    makeiconURL = function makeiconURL(data,options,size=32){
+      let cachekey=JSON.stringify({icon:data.icon,icons:data.icons,icon_size:data.icon_size,rendered_size:size});
+      if(cachekey in iconcache){
+        console.log(cachekey);
+        return makePromise(iconcache[cachekey]);
+      }
+      let canvas=window.__imagestuff__.getCanvas(size,size);
+      return pool.run([canvas,data,options,size],{transfer:[canvas]}).then((url)=>{
+        iconcache[cachekey]=url;
+        return url;
+      });
+    }
   }
 }
 
